c8947c12b64fffdb10ef9fc27227f4a130ba02cd
Chima Precious <ositachima.co@gmail.com>
2025-01-20 15:43:30 +0000

    use base-compat runtime

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,15 @@
-void main() {
-  print('Hello World');
+import 'dart:io';
+
+void main() async {
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, 8080);
+
+  server.listen((HttpRequest req) async {
+    final res = req.response;
+    res
+      ..headers.contentType = ContentType.text
+      ..write('Hello World');
+    await res.close();
+  });
+
+  print('Server started listening on ${server.port}');
 }
